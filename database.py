8cfb703296b5fd1530396ef42277f06163d7093d
Ching Chan <65863602+themodern@users.noreply.github.com>
2023-12-15 02:26:31 +1100

    first render api

--- database.py
+++
@@ -1,8 +0,0 @@
-from model import ToDo
-#MongoDB Driver
-import motor.motor_asyncio
-
-
-
-
-
--- model.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class ToDo(BaseModel):
-    title: str
-    description: str
